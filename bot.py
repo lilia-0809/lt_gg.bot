878277a9e245af6857a7712aaddc1f876a394ab1
lilia-0809 <gurniklilia@gmail.com>
2025-10-07 21:15:55 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import telebot
+import random
+import os
+print(os.listdir('images'))
+
+bot = telebot.TeleBot('8253309841:AAGfHqHtSwzZJuj4FYtxdnZcrOiZz-AKz1Y')
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, "Привет! Используй команду /MS или /Alfedov, чтобы получить мем!")
+
+@bot.message_handler(commands=['Alfedov'])
+def send_mem(message):
+    with open('image/imageM1.png', 'rb') as f:  
+        bot.send_photo(message.chat.id, f)  
+
+@bot.message_handler(commands=['MS'])
+def send_mem(message):
+    img_name = random.choice(os.listdir('image'))  # Случайным образом выбираем изображение
+    with open(f'image/{img_name}', 'rb') as f:
+        # Отправляем фото, выбранное случайным образом
+        bot.send_photo(message.chat.id, f)
+
+
+
+bot.polling()
